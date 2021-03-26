1228278acff3a895079f289f30f6f175a571e10d
Kiril Kirilov <kirilkirilov@abv.bg>
2021-03-27 01:10:51 +0200

    Add files via upload

---
+++ Task01_Vehicles/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task01_Vehicles
+{
+    public class Car : Vehicle
+    {
+        
+
+        public Car(double fuelQuantity, double fuelConsumption) : base (fuelQuantity, fuelConsumption)
+        {
+                
+        }
+    }
+}
---
+++ Task01_Vehicles/Truck.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task01_Vehicles
+{
+    public class Truck : Vehicle
+    {
+        
+
+        public Truck(double fuelQuantity, double fuelConsumption) : base(fuelQuantity, fuelConsumption)
+        {
+
+        }
+
+    }
+}
