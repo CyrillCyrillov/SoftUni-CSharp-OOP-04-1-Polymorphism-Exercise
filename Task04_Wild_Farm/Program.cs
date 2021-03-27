d697c043fb51bcc606d66d65d5546d1cc1fac3e9
Kiril Kirilov <kirilkirilov@abv.bg>
2021-03-27 23:38:15 +0200

    Add files via upload

---
+++ Task04_Wild_Farm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task04_Wild_Farm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
